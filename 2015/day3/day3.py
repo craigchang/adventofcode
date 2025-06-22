1b505c5853be924bdee33aeb3dea4196c5ea8e34
Craig Chang <craigchang@gmail.com>
2025-06-22 10:25:41 -0700

    2015 day3

--- 2015/day3/day3.py
+++ 2015/day3/day3.py
@@ -1,41 +1,31 @@
 # https://adventofcode.com/2015/day/3
 
-def moveDir(move, x, y):
-  if move == '^':
-    y += 1
-  elif move == 'v':
-    y -= 1
-  elif move == '>':
-    x += 1
-  elif move == '<':
-    x -= 1
-  return (x,y)
+def moveDir(m, x, y):
+    if m == '<':    return x-1,y
+    elif m == '>':  return x+1,y
+    elif m == '^':  return x,y+1
+    elif m == 'v':  return x,y-1
 
-def part1():
-  with open("2015/day3/input.txt", "r") as f:
-    santa, x, y = {(0,0)}, 0, 0
-    print(santa)
-    for move in f.readline():
-      x, y = moveDir(move, x, y)
-      santa.add((x, y))
-    print(santa)
-    print(len(santa))
+# part 1
+with open("2015/day3/input.txt", "r") as f:
+    moves = f.readline()
+    visited, x, y = {(0,0)},0,0
+    for m in moves:
+        x,y = moveDir(m,x,y)
+        visited.add((x,y))
+    print(len(visited))
 
-def part2():
-  with open("2015/day3/input.txt", "r") as f:
-    santa, x1, y1 = {(0,0)}, 0, 0
-    robo, x2, y2 ={(0,0)}, 0, 0
-    turn = 0 # 0 for santa, 1 for robo
-    for move in f.readline():
-      if (not turn):
-        x1, y1 = moveDir(move, x1, y1)
-        santa.add((x1, y1))
-      else:
-        x2, y2 = moveDir(move, x2, y2)
-        robo.add((x2,y2))
-      turn = not turn
-    total = santa.union(robo)
-    print(len(total))
-
-part1()
-part2()
+# part 2
+with open("2015/day3/input.txt", "r") as f:
+    moves = f.readline()
+    visited, sX, sY, rX, rY = {(0,0)},0,0,0,0
+    turn = 1
+    for m in moves:
+        if turn:
+            sX, sY = moveDir(m,sX,sY)
+            visited.add((sX,sY))
+        else:
+            rX, rY = moveDir(m,rX,rY)
+            visited.add((rX,rY))
+        turn = not turn
+    print(len(visited))
